da250f212ac4a67a68ab6973ce16c85590948d87
JaviCuri <jcuripan2022@alu.uct.cl>
2023-11-23 23:28:16 -0300

    EZ

--- src/graphproyect/GraphProyect.java
+++ src/graphproyect/GraphProyect.java
@@ -14,7 +14,7 @@ public class GraphProyect {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("ez");
     }
     
 }
