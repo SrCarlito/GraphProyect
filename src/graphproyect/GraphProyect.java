428883ef4ca5ff796716d8bb02afd6e544a46f65
JaviCuri <jcuripan2022@alu.uct.cl>
2023-11-24 16:10:07 -0300

    Probando lecturaJSON

--- src/graphproyect/GraphProyect.java
+++ src/graphproyect/GraphProyect.java
@@ -4,17 +4,52 @@
  */
 package graphproyect;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  *
  * @author lolsg
  */
 public class GraphProyect {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("ez");
+        
+        String currentPath = "C:/Users/javic/Desktop/trabajos/2023 2 semestre/Teoria de Grafo/GraphProyect";
+        String rutaArchivo = currentPath + "/archivoJson/example.json";
+        
+     try{
+         //
+         String contenido = new String(Files.readAllBytes(Paths.get(rutaArchivo)));
+         
+         JSONObject json = new JSONObject(contenido);
+         
+         JSONObject graph = json.getJSONObject("graph");
+         JSONArray nodes = graph.getJSONArray("nodes");
+         
+         Scanner ZZZ = new Scanner(System.in);
+         
+         System.out.print("Ingresa ID del nodo");
+         int NodeId = Scanner.nextInt();
+         
+         
+         
+         
+         
+         
+         
+         
+     }catch(IOException z){
+         z.printStackTrace(); // error
+     }
+     
     }
     
 }
