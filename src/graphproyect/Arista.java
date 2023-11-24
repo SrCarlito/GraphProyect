3a94ae56acec5e0e040d0d9a5c571d873356a989
Carlito__ <106630043+SrCarlito@users.noreply.github.com>
2023-11-24 12:27:49 -0300

    Clase Arista

---
+++ src/graphproyect/Arista.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package graphproyect;
+
+/**
+ *
+ * @author lolsg
+ */
+public class Arista {
+
+    private int destino;
+    private int peso;
+
+    public Arista(int destino, int peso) {
+        this.destino = destino;
+        this.peso = peso;
+    }
+
+    public int getDestino() {
+        return destino;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+}
